40f6eba6b1c6afb8c29022f6d6e43aa54a1f7d3a
sergiuosvat <sergiuosvat@yahoo.com>
2023-05-13 08:36:56 +0300

    log in screen

---
+++ frontend/src/components/LogIn/LogIn.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import styles from './LogIn.module.css';
+import auth from './auth';
+
+interface LoginProps {
+  onLogin: (username: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    auth.username = username;
+    auth.password = password;
+    onLogin(username, password);
+  };
+
+  return (
+    <div className={styles.loginContainer}>
+      <img className = {styles.logo} src="/images/blue_text.svg" alt="Logo" />
+      <form onSubmit={handleLogin}>
+        <div className="formgroup">
+          <input
+            className={styles.input}
+            type="text"
+            id="username"
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className={styles.formgroup}>
+          <input
+            className={styles.input}
+            type="password"
+            id="password"
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className={styles.loginButton} type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/components/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div>
+      <img src="/images/blue_logo.svg" alt="logo" />
+    </div>
+  );
+};
+
+export default SplashScreen;
