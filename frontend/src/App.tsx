d8ce55674ca1179ca86d042a3eb95dbd1dfcd230
sergiuosvat <sergiuosvat@yahoo.com>
2023-05-13 22:32:08 +0300

    fixed splashscreen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,53 @@
-import { Fragment } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-
-import CssBaseline from '@mui/material/CssBaseline';
-
-import { withErrorHandler } from '@/error-handling';
-import AppErrorBoundaryFallback from '@/error-handling/fallbacks/App';
-import Pages from '@/routes/Pages';
-import Notifications from '@/sections/Notifications';
-import SW from '@/sections/SW';
-import Sidebar from '@/sections/Sidebar';
-import Login from './components/LogIn/LogIn';
-import SplashScreen from './components/SplashScreen/SplashScreen';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { withErrorHandler } from "@/error-handling";
+import AppErrorBoundaryFallback from "@/error-handling/fallbacks/App";
+import Pages from "@/routes/Pages";
+import Notifications from "@/sections/Notifications";
+import SW from "@/sections/SW";
+import Sidebar from "@/sections/Sidebar";
+import Login from "./components/LogIn/LogIn";
+import SplashScreen from "./components/SplashScreen/SplashScreen";
 
 function App() {
-  const [showSplashScreen, setShowSplashScreen] = React.useState(true);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setShowSplashScreen(false);
-    }, 2000);
-  }, []);
-
-  const [loggedIn, setLoggedIn] = React.useState(
-    sessionStorage.getItem('isLoggedIn') === 'true'
+  const [loggedIn, setLoggedIn] = useState(
+    sessionStorage.getItem("isLoggedIn") === "true"
   );
+  const [showSplashScreen, setShowSplashScreen] = useState(!loggedIn);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      setTimeout(() => {
+        setShowSplashScreen(false);
+      }, 2000);
+    }
+  }, [loggedIn]);
 
   const handleLogin = () => {
     setLoggedIn(true);
-    sessionStorage.setItem('isLoggedIn', 'true');
+    sessionStorage.setItem("isLoggedIn", "true");
   };
 
   return (
-    <Fragment>
+    <>
       <CssBaseline />
       {showSplashScreen ? (
         <SplashScreen />
       ) : loggedIn ? (
-        <Fragment>
+        <>
           <Notifications />
           <SW />
           <BrowserRouter>
             <Sidebar />
             <Pages />
           </BrowserRouter>
-        </Fragment>
+        </>
       ) : (
         <Login onLogin={handleLogin} />
       )}
-    </Fragment>
+    </>
   );
 }
 
 export default withErrorHandler(App, AppErrorBoundaryFallback);
-
-
