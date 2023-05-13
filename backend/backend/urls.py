35d173ca1fb59c7f15893914b660d7d1fa904d88
Alex <alexmihoc@yahoo.com>
2023-05-13 12:49:39 +0300

    added auth to backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from api import views
+import rest_auth
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -12,5 +13,6 @@ router.register(r'polygons', views.PolygonViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('rest-auth', include('rest_auth.urls')),
 ]
--- frontend/src/components/LogIn/LogIn.tsx
+++ frontend/src/components/LogIn/LogIn.tsx
@@ -17,10 +17,40 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
     onLogin(username, password);
   };
 
+  const handleUsernameChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+    // Make API request to login endpoint
+    fetch('http://127.0.0.1:8000/api-auth/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(error);
+      });
+  };
+
   return (
     <div className={styles.loginContainer}>
       <img className = {styles.logo} src="/images/blue_text.svg" alt="Logo" />
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleSubmit}>
         <div className="formgroup">
           <input
             className={styles.input}
